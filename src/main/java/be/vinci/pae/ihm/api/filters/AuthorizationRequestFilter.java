0ec0886c083fe37012acf39e43ca0a971fcf21d2
soulaymanegharroudi <soulaymane.gharroudi@student.vinci.be>
2022-03-07 14:27:50 +0100

    Refactor code according to jenkins build #36

--- src/main/java/be/vinci/pae/ihm/api/filters/AuthorizationRequestFilter.java
+++ src/main/java/be/vinci/pae/ihm/api/filters/AuthorizationRequestFilter.java
@@ -1,6 +1,5 @@
 package be.vinci.pae.ihm.api.filters;
 
-import be.vinci.pae.business.ucc.MemberUCC;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
@@ -16,22 +15,22 @@ public class AuthorizationRequestFilter implements ContainerRequestFilter {
   //  private final JWTVerifier jwtVerifier = JWT.require(this.jwtAlgorithm).withIssuer("auth0")
   //      .build();
   //  @Inject
-  private MemberUCC memberUCC;
+  //  private MemberUCC memberUCC;
 
 
   @Override
   public void filter(ContainerRequestContext requestContext) throws IOException {
-//    String token = requestContext.getHeaderString("Authorization");
-//    if (token == null) {
-//      requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
-//          .entity("A token is needed to access this resource").build());
-//    } else {
-//      DecodedJWT decodedToken = null;
-//      try {
-//        decodedToken = this.jwtVerifier.verify(token);
-//      } catch (Exception e) {
-//        throw new TokenDecodingException(e);
-//      }
+    //    String token = requestContext.getHeaderString("Authorization");
+    //    if (token == null) {
+    //      requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+    //          .entity("A token is needed to access this resource").build());
+    //    } else {
+    //      DecodedJWT decodedToken = null;
+    //      try {
+    //        decodedToken = this.jwtVerifier.verify(token);
+    //      } catch (Exception e) {
+    //        throw new TokenDecodingException(e);
+    //      }
     //    }
   }
 }
