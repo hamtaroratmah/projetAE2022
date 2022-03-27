e9aae0f14c037560e4905877d23bbb41abbc1ba5
Guillaume <guillaume.bouillon@student.vinci.be>
2022-03-27 18:34:11 +0200

    refactor commit 285

--- src/main/java/be/vinci/pae/ihm/api/ItemResource.java
+++ src/main/java/be/vinci/pae/ihm/api/ItemResource.java
@@ -8,16 +8,15 @@ import be.vinci.pae.business.domain.interfacesdto.TypeDTO;
 import be.vinci.pae.business.ucc.ItemUCC;
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
 import java.util.List;
 
 @Path("/item")
