90990e0d632691bf9eaeb959dd5b9cf8db0347ea
soulaymanegharroudi <soulaymane.gharroudi@student.vinci.be>
2022-03-27 10:12:06 +0200

    fix navbar bug

--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -17,12 +17,12 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Main {
 
-  // Base URI the Grizzly HTTP server will listen on
-  public static final String BASE_URI = Config.getProperty("BaseUri");
-
   static {
     Config.load("dev.properties");
   }
+
+  // Base URI the Grizzly HTTP server will listen on
+  public static final String BASE_URI = Config.getProperty("BaseUri");
 
   /**
    * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -51,8 +51,8 @@ public class Main {
   public static void main(String[] args) throws IOException {
     final HttpServer server = startServer();
 
-    System.out.println(String.format("Jersey app started with WADL available at "
-        + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+    System.out.printf("Jersey app started with WADL available at "
+        + "%sapplication.wadl\nHit enter to stop it...%n", BASE_URI);
     System.in.read();
 
     server.stop();
