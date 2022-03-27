84036c9dfb64992693103532164893738e0b0266
Guillaume <guillaume.bouillon@student.vinci.be>
2022-03-27 14:30:16 +0200

    hotfix

--- src/main/java/be/vinci/pae/ihm/api/ItemResource.java
+++ src/main/java/be/vinci/pae/ihm/api/ItemResource.java
@@ -6,16 +6,12 @@ import be.vinci.pae.business.domain.interfacesdto.ItemDTO;
 import be.vinci.pae.business.domain.interfacesdto.MemberDTO;
 import be.vinci.pae.business.domain.interfacesdto.TypeDTO;
 import be.vinci.pae.business.ucc.ItemUCC;
+import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
 import java.util.List;
 
 @Path("/item")
@@ -31,7 +27,7 @@ public class ItemResource {
    * Get offered items from databased sorted by date_offer or type.
    */
   @GET
-  @Path("/")
+  @Path("/getLastOfferedItems")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public List<ItemDTO> getLastOfferedItems() {
@@ -65,8 +61,6 @@ public class ItemResource {
 
   /**
    * Get a specified item according to its id.
-   *
-   * @param idItem item's id that we want more details
    */
   @POST
   @Path("/createItem")
@@ -74,24 +68,8 @@ public class ItemResource {
   @Produces(MediaType.APPLICATION_JSON)
   public ItemDTO createItem(JsonNode json) {
     if (!json.hasNonNull("type") || !json.hasNonNull("description") || !json.hasNonNull(
-        "availabilities")
-        || !json.hasNonNull("item_condition") || !json.hasNonNull("id_offering_member")) {
-      throw new WebApplicationException("Lack of informations", Response.Status.BAD_REQUEST);
-    }
-    MemberDTO offeringMember = domainFactory.getMember();
-    offeringMember.setIdMember(json.get("id_offering_member").asInt());
-
-  /**
-   * Get a specified item according to its id.
-   */
-  @POST
-  @Path("/createItem")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-  public ItemDTO createItem(JsonNode json) {
-    if (!json.hasNonNull("type") || !json.hasNonNull("description") || !json.hasNonNull(
-        "availabilities")
-        || !json.hasNonNull("item_condition") || !json.hasNonNull("id_offering_member")) {
+            "availabilities")
+            || !json.hasNonNull("item_condition") || !json.hasNonNull("id_offering_member")) {
       throw new WebApplicationException("Lack of informations", Response.Status.BAD_REQUEST);
     }
     MemberDTO offeringMember = domainFactory.getMember();
