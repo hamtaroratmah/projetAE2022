abbdd15e1b62b0c9056b88ca37fe78b48f395439
Guillaume <guillaume.bouillon@student.vinci.be>
2022-04-13 13:39:18 +0200

    refactor checkStyle

--- src/main/java/be/vinci/pae/business/ucc/MemberUCCImpl.java
+++ src/main/java/be/vinci/pae/business/ucc/MemberUCCImpl.java
@@ -133,14 +133,14 @@ public class MemberUCCImpl implements MemberUCC {
   public MemberDTO register(MemberDTO member) {
     try {
       dalServices.startTransaction();
-      if (getOneByUsername(member.getUsername()) != null){
+      if (getOneByUsername(member.getUsername()) != null) {
         throw new IllegalArgumentException("L'utilisateur existe déjà !");
       }
-        Member memberBiz = (Member) member;
-        String hashPass = memberBiz.hashPassword(member.getPassword());
-        member.setPassword(hashPass);
-        memberDao.insertMember(member);
-        return (MemberDTO) member;
+      Member memberBiz = (Member) member;
+      String hashPass = memberBiz.hashPassword(member.getPassword());
+      member.setPassword(hashPass);
+      memberDao.insertMember(member);
+      return (MemberDTO) member;
     } catch (Exception e) {
       dalServices.rollbackTransaction();
       throw new FatalException(e.getMessage());
