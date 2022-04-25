2e180503929a76cd233cc329300472c1e2576dc2
Guillaume <guillaume.bouillon@student.vinci.be>
2022-04-25 15:27:31 +0200

    fetch #1

--- webapp/src/utils/displayModule/members.js
+++ webapp/src/utils/displayModule/members.js
@@ -1,9 +1,10 @@
-import {confirmInscription} from "../api/member";
+import {confirmInscription, denyInscription} from "../api/member";
 
 function displayInscriptions(inscriptions) {
     const listInscriptionsPage = document.querySelector("#listInscriptionsPage");
     for (let i = 0; i < inscriptions.length; i++) {
-        listInscriptionsPage.innerHTML += `
+        if (inscriptions[i].state === "pending") {
+            listInscriptionsPage.innerHTML += `
         <div id="inscriptionPending" class="receptionInscriptionParent">
           <div class="receptionInscriptionChild1">
           <form id="confirmForm">
@@ -26,22 +27,56 @@ function displayInscriptions(inscriptions) {
           </form>
           </div>
             <div class="receptionInscriptionChild2">
-             <form id="denyForm${i}">
-                <input type="submit" value="X">
+             <form id="denyForm">
+                <input id="deny${i}" type="submit" value="X">
                 </form>
             </div>
         </div>
       `;
+        } else {
+            listInscriptionsPage.innerHTML += `
+        <div id="inscriptionPending" class="receptionInscriptionParent">
+          <div class="receptionInscriptionChild1">
+          <form id="confirmForm">
+            <div class="receptionInscriptionGrandChild">
+              <p>
+                ${inscriptions[i].username}
+                ${inscriptions[i].lastName} 
+                ${inscriptions[i].firstName}
+              </p>
+            </div>
+            <div class="receptionInscriptionGrandChild">
+                state : ${inscriptions[i].state}
+            </div>
+            <div class="receptionInscriptionGrandChild">
+                <input type="checkbox" id="isAdmin${i}" name="isAdmin${i}">
+            </div>
+            <div class="receptionInscriptionGrandChild">
+                <input id ="confirm${i}" type="submit" value="Confirmer inscription">
+            </div>
+          </form>
+          </div>
+        </div>
+        `;
+        }
     }
 
     for (let i = 0; i < inscriptions.length; i++) {
-        // Confirm inscription
+
         const isAdmin = document.getElementById("isAdmin"+i);
         const buttonConfirm = document.getElementById("confirm"+i);
+        const buttonDeny = document.getElementById("deny"+i);
 
+        // Confirm inscription
         buttonConfirm.addEventListener("click",async (e) => {
             e.preventDefault();
             await confirmInscription(inscriptions[i].username,isAdmin.checked);
+        })
+
+        // Deny inscription
+        buttonDeny.addEventListener("click",async (e) => {
+            e.preventDefault();
+            await denyInscription(inscriptions[i].username);
         })
     }
 }
